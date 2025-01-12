101846656eebf6d38894f5d754adff5bea29db16
Hamid Ait <hmdaitbjj@gmail.com>
2025-01-12 19:00:34 +0100

    changes in experience section

--- js_index.js
+++ js_index.js
@@ -1,72 +1,111 @@
-const experienceSection = Vue.createApp({
-    data(){
-        return {
-            Experience: {
-                expleo: {
-                    title: "Software Validation Engineer",
-                    date: "September 2023 - Present",
-                    details: [
-                        "Wrote test automation scripts for ADAS systems {UPA/FKP, DMS, and SVS}, reducing manual test time by 40% and increasing test coverage by configuring the test environment on a HIL bench to ensure accurate and efficient testing.",
-                        "Conducted software tests directly on vehicles, ensuring the functionality and compliance of ADAS software flashed into the ECUs, using Renault's internal tools such as DDT2000, ADaPT, Jira, CodeBeamer, as well as Vector tools (CANoe, CANape, etc.)",
-                        "Developed CAPL scripts for a Gateway in a SDV architecture development project, based on an existing architecture. This project involved monitoring all frames of the CAN buses from the old architecture to ensure a smooth and secure transition.",
-                        "Wrote and presented detailed weekly reports to clients and internal teams, providing full transparency on project progress."
-                    ]
-                },
-                renault:{
-                    
-                        title: "Internship Trainee",
-                        date: "February. 2023 - June. 2023 ",
-                        logo: "https://companieslogo.com/img/orig/CAP.PA-9b4110b0.png?t=1720244491",
-                        details: [
-                            "Designed and developed a centralized supervision interface using Python, JavaScript, HTML, CSS, enabling real-time monitoring of all Renault test benches worldwide, improved user responsiveness through a system of customized alerts leveraging a client-server architecture with WebSocket communication.",
-                            "Built a global database aggregating the capabilities of all Renault test benches, ensuring efficient management of critical information with a focus on scalability and reliability.",
-                            "Developed APIs using Python and integrated them with test bench systems for real-time data collection from PCs, ensuring reliable and fast data reporting via Socket communication.",
-                            "Created a web-based user interface with advanced features (filters, subscriptions, notifications) using JavaScript, HTML, CSS, for an optimized user experience and easy access to relevant data."
-                        ]
-                    },
-                capgemini:{
-                    title: "Mechatronics Engineer",
-                        date: "February 2022 - August 2022",
-                        details: [
-                            "Led and monitored the implementation of ADAS components, particularly the Driver Monitoring System (DMS), across various vehicle projects, ensuring seamless integration in a constrained environment.",
-                            "Managed CAN frames in collaboration with the supplier Bosch, including the officialization of software/hardware and the management of CAN databases.",
-                            "Validated product/process compliance according to B21 standards and connector standards, ensuring adherence to quality and safety requirements.",
-                            "Facilitated technical meetings and provided regular project progress reports, ensuring smooth communication between business and project teams."
-                        ]
-                    },
-                    valeo:
-                    {
-                        title: "Pre-employment internship",
-                        date: "November 2021 - February 2022",
-                        details: [
-                            "Developed comprehensive technical specification documents outlining precise equipment requirements for the manufacturing of printed circuit boards, specifically for automotive lighting systems and other electronic devices, ensuring optimal performance and alignment with industry standards.",
-                            "Coordinated with suppliers for the selection and procurement of equipment, including organizing sourcing committees (SOCO) for budgets exceeding €30,000, ensuring the best quality/price/delivery ratio.",
-                            "Led project kickoff meetings, ensuring adherence to industrial schedules and compliance with technical specifications.",
-                            "Monitored and validated equipment post-installation, managing non-conformities and drafting detailed reports, facilitating the transition to the production phase (SOP)."
-                                ],
-                        visible: false // Initially hidden
-                    },
-                    treves:
-                    {
-                        title: "Graduation internship",
-                        date: "Mars 2021 - July. 2021 ",
-                        details: [
-                            "Analysis of requirements and feasibility study of the production process automation project, including the definition of objectives and planning via a Gantt chart.",
-                            "Design and modeling of the automation system components, followed by the development of a detailed specification document, then communication and coordination with suppliers for manufacturing.",
-                            "Programming in Ladder on SIMATIC Step7 and program simulation, including the selection of the PLC, sensors, and actuators needed for process automation.",
-                            "Assembly and testing of the system on-site, followed by adjustments to ensure optimal performance and compliance with the initial objectives."
-                                ],
-                        visible: false // Initially hidden
-                    }
-            }
-                
-        }
+// ecperience data 
+const experiences = [
+    {
+      company: "Expleo",
+      logo: "images/logos/expleo-group-logo.svg",
+      role: "Software Validation Engineer",
+      startDate: "September. 2023",
+      endDate: "Mars. 2025",
+      summary: "Wrote test automation scripts for ADAS systems {UPA/FKP, DMS, and SVS}, reducing manual test time by 40% and increasing ...",
+      details: [
+        "- Wrote test automation scripts for ADAS systems {UPA/FKP, DMS, and SVS}, reducing manual test time by 40% and increasing test coverage by configuring the test environment on a HIL bench to ensure accurate and efficient testing.",
+        "- Conducted software tests directly on vehicles, ensuring the functionality and compliance of ADAS software flashed into the ECUs, using Renault's internal tools such as DDT2000, ADaPT, Jira, CodeBeamer, as well as Vector tools (CANoe, CANape, etc.)",
+        "- Developed CAPL scripts for a Gateway in a SDV architecture development project, based on an existing architecture. This project involved monitoring all frames of the CAN buses from the old architecture to ensure a smooth and secure transition.",
+        "- Wrote and presented detailed weekly reports to clients and internal teams, providing full transparency on project progress."
+    ]
     },
-    methods:{
-    }
+    {
+      company: "Reneault Group",
+      logo: "images/logos/renault-logo.png",
+      role: "Internship Trainee",
+      startDate: "February. 2023 ",
+      endDate: "June. 2023",
+      summary: "Designed and developed a centralized supervision interface using Python, JavaScript, HTML, CSS, enabling real-time ...",
+      details: [
+        "- Designed and developed a centralized supervision interface using Python, JavaScript, HTML, CSS, enabling real-time monitoring of all Renault test benches worldwide, improved user responsiveness through a system of customized alerts leveraging a client-server architecture with WebSocket communication.",
+        "- Built a global database aggregating the capabilities of all Renault test benches, ensuring efficient management of critical information with a focus on scalability and reliability.",
+        "- Developed APIs using Python and integrated them with test bench systems for real-time data collection from PCs, ensuring reliable and fast data reporting via Socket communication.",
+        "- Created a web-based user interface with advanced features (filters, subscriptions, notifications) using JavaScript, HTML, CSS, for an optimized user experience and easy access to relevant data."
+    ]
+    },
+    {
+        company: "Capgrmini Engineering",
+        logo: "images/logos/Capgemini-Logo.png",
+        role: "Mechatronic development consultant",
+        startDate: "February. 2023 ",
+        endDate: "June. 2023",
+        summary: "Led and monitored the implementation of ADAS components, particularly the Driver Monitoring System (DMS) ...",
+        details: [
+            "- Led and monitored the implementation of ADAS components, particularly the Driver Monitoring System (DMS), across various vehicle projects, ensuring seamless integration in a constrained environment.",
+            "- Managed CAN frames in collaboration with the supplier Bosch, including the officialization of software/hardware and the management of CAN databases.",
+            "- Validated product/process compliance according to B21 standards and connector standards, ensuring adherence to quality and safety requirements.",
+            "- Facilitated technical meetings and provided regular project progress reports, ensuring smooth communication between business and project teams."
+        ]
+      },
+      {
+        company: "Valeo",
+        logo: "images/logos/Valeo_Logo.png",
+        role: "Pre-employment internship",
+        startDate: "November. 2021",
+        endDate: "February. 2022",
+        summary: "Developed comprehensive technical specification documents outlining precise equipment requirements for the ...",
+        details: [
+            "- Developed comprehensive technical specification documents outlining precise equipment requirements for the manufacturing of printed circuit boards, specifically for automotive lighting systems and other electronic devices, ensuring optimal performance and alignment with industry standards.",
+            "- Coordinated with suppliers for the selection and procurement of equipment, including organizing sourcing committees (SOCO) for budgets exceeding €30,000, ensuring the best quality/price/delivery ratio.",
+            "- Led project kickoff meetings, ensuring adherence to industrial schedules and compliance with technical specifications.",
+            "- Monitored and validated equipment post-installation, managing non-conformities and drafting detailed reports, facilitating the transition to the production phase (SOP)."
+                ]
+      },
+      {
+        company: "Threves",
+        logo: "images/logos/TREVES-logo.png",
+        role: "Graduation internship",
+        startDate: "Mars. 2021",
+        endDate: "July. 2021",
+        summary: "Analysis of requirements and feasibility study of the production process automation project, including the ...",
+        details: [
+            "- Analysis of requirements and feasibility study of the production process automation project, including the definition of objectives and planning via a Gantt chart.",
+            "- Design and modeling of the automation system components, followed by the development of a detailed specification document, then communication and coordination with suppliers for manufacturing.",
+            "- Programming in Ladder on SIMATIC Step7 and program simulation, including the selection of the PLC, sensors, and actuators needed for process automation.",
+            "- Assembly and testing of the system on-site, followed by adjustments to ensure optimal performance and compliance with the initial objectives."
+                ]
+      },
+  ];
 
-});
-experienceSection.mount('#experience');
+const slider = document.getElementById('experience-slider');
+
+  experiences.forEach(exp => {
+    const card = document.createElement('div');
+    card.className = 'experience-card';
+    card.innerHTML = `
+      <div class="swiper-slide">
+      <img src="${exp.logo}" alt="${exp.company} logo">
+      <h3>${exp.company}</h3>
+      <p><strong>${exp.role}</strong></p>
+      <p>${exp.startDate} - ${exp.endDate}</p>
+      <p>${exp.summary}</p>
+      </div>
+    `;
+    card.addEventListener('click', () => showPopup(exp.company, exp.details));
+    slider.appendChild(card);
+  });
+
+  function showPopup(title, details) {
+    document.getElementById('popup-title').innerText = "Tasks :";
+  
+    // Join the details array with HTML paragraph tags and set as innerHTML
+    const formattedDetails = details.map(item => `<p>${item}</p>`).join('');
+    document.getElementById('popup-details').innerHTML = formattedDetails;
+  
+    document.getElementById('experience-popup').style.display = 'flex';
+  }
+  
+
+  function closePopup() {
+    document.getElementById('experience-popup').style.display = 'none';
+  }
+
+  
 
 const aboutSection = Vue.createApp({
     data(){
@@ -128,8 +167,7 @@ const skillsSection = Vue.createApp({
     methods:{
 
     }
-})
-
+});
 skillsSection.mount('#skills');
 
 const menuIcone = document.querySelector('#menu-icon');
@@ -138,23 +176,6 @@ const navLinks = document.querySelector('.nav-links');
 menuIcone.onclick = ()=>{
     navLinks.classList.toggle('active')
 }
-
-//Swwper code 
-new Swiper('.swiper', {
-    loop: true,
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      dynamicBullets: true,
-    },
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  });
-
   
 function openVideo1() {
     // Path to Respons assiste video
@@ -200,4 +221,4 @@ const typed = new Typed('.multiple-text',{
     backSpeed: 70,
     backDelay: 1500,
     loop: true,
-})
+});
